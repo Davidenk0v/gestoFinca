4745646acb94e791c387031215911ffccac8948f
David <davidalvper@gmail.com>
2025-01-14 19:40:22 +0000

    add all pages and added landing page

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,98 @@
+const ContactForm = () => {
+  return (
+    <form className="max-w-sm mx-auto">
+      <div>
+        <label
+          htmlFor="small-input"
+          className="block m-2 text-sm font-medium text-gray-900 :text-white"
+        >
+          Nombre*
+        </label>
+        <input
+          type="text"
+          id="small-input"
+          className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-green-700 focus:border-green-700 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white :focus:ring-green-700 :focus:border-green-700"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="small-input"
+          className="block m-2 text-sm font-medium text-gray-900 :text-white"
+        >
+          Teléfono*
+        </label>
+        <input
+          type="text"
+          id="small-input"
+          className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-green-700 focus:border-green-700 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white :focus:ring-green-700 :focus:border-green-700"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="small-input"
+          className="block m-2 text-sm font-medium text-gray-900 :text-white"
+        >
+          Correo electrónico*
+        </label>
+        <input
+          type="email"
+          id="small-input"
+          className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-green-700 focus:border-green-700"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="small-input"
+          className="block m-2 text-sm font-medium text-gray-900 :text-white"
+        >
+          Asunto*
+        </label>
+        <input
+          type="text"
+          id="small-input"
+          className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-green-700 focus:border-green-700"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="small-input"
+          className="block m-2 text-sm font-medium text-gray-900 :text-white"
+        >
+          Mensaje*
+        </label>
+        <textarea
+          id="small-input"
+          className="mb-2 h-20 block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-green-700 focus:border-green-700"
+        />
+      </div>
+
+      <div className="flex items-center">
+        <input
+          id="link-checkbox"
+          type="checkbox"
+          value=""
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+        />
+        <label
+          htmlFor="link-checkbox"
+          className="ms-2 mb-2 text-sm font-medium text-gray-900"
+        >
+          Consiento el uso de mis datos para los fines indicados en la{" "}
+          <a
+            href="/politica-de-privacidad"
+            className="text-blue-600 :text-blue-500 hover:underline"
+          >
+            política de privacidad*
+          </a>
+          .
+        </label>
+      </div>
+      <div className="flex justify-center m-2">
+        <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:ring-blue-300">
+          Contactar
+        </button>
+      </div>
+    </form>
+  );
+};
+export default ContactForm;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,99 +47,19 @@ const Header = () => {
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          <div className="relative">
-            <button
-              type="button"
-              className="peer flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 focus:outline-none"
-              aria-expanded="false"
-            >
-              Servicios
-              <svg
-                className="size-5 flex-none text-green-700"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-            <div className="hidden peer-focus:block absolute left-8 top-full z-20 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-slate-200 shadow-lg ring-1 ring-gray-900/5">
-              <div className="p-4">
-                <div className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50">
-                  <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                    <svg
-                      className="size-6 text-gray-600 group-hover:text-indigo-600"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
-                      />
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="flex-auto">
-                    <a
-                      href="/administracion"
-                      className="block font-semibold text-gray-900"
-                    >
-                      Administración de fincas
-                      <span className="absolute inset-0"></span>
-                    </a>
-                    <p className="mt-1 text-gray-600">
-                      Gestionamos fincas como si fueran nuestras
-                    </p>
-                  </div>
-                </div>
-                <div className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50">
-                  <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                    <svg
-                      className="size-6 text-gray-600 group-hover:text-indigo-600"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59"
-                      />
-                    </svg>
-                  </div>
-                  <div className="flex-auto">
-                    <a
-                      href="/alquileres"
-                      className="block font-semibold text-gray-900"
-                    >
-                      Gestión de alquileres vacacionales
-                      <span className="absolute inset-0"></span>
-                    </a>
-                    <p className="mt-1 text-gray-600">
-                      Sácale el mayor partido a tu vivienda vacacional
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <a
+            href="/administracion-fincas"
+            className="text-sm/6 border-b-2 border-transparent font-semibold text-gray-900 focus:border-b-2 focus:border-b-green-700"
+          >
+            Administración de fincas
+          </a>
+
+          <a
+            href="/alquileres-vacacionales"
+            className="text-sm/6 border-b-2 border-transparent font-semibold text-gray-900 focus:border-b-2 focus:border-b-green-700"
+          >
+            Alquileres vacacionales
+          </a>
 
           <a
             href="/nosotros"
